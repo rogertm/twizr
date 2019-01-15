ccaf97835adce698e5a0c2a6e009862c52192aaa
RogerTM <roger213tm@gmail.com>
2019-01-15 15:54:00 -0500

    Commit inicial para Twizr

--- functions.php
+++ functions.php
@@ -15,31 +15,31 @@
  *
  * @since Twenty'em Child 1.0
  */
-function t_em_child_setup(){
+function twizr_setup(){
 	// Make Twenty'em Child available for translation.
-	load_child_theme_textdomain( 't_em_child', get_stylesheet_directory() . '/languages' );
+	load_child_theme_textdomain( 'twizr', get_stylesheet_directory() . '/languages' );
 }
-add_action( 'after_setup_theme', 't_em_child_setup' );
+add_action( 'after_setup_theme', 'twizr_setup' );
 
 /**
  * Enqueue and register all css and js
  *
  * @since Twenty'em Child 1.0
  */
-function t_em_child_enqueue(){
-	wp_register_style( 't_em_child-', t_em_get_css( 'theme', T_EM_CHILD_THEME_DIR_PATH .'/css', T_EM_CHILD_THEME_DIR_URL .'/css' ), '', t_em_theme( 'Version' ), 'all' );
-	wp_enqueue_style( 't_em_child-' );
+function twizr_enqueue(){
+	wp_register_style( 'twizr-', t_em_get_css( 'theme', T_EM_CHILD_THEME_DIR_PATH .'/css', T_EM_CHILD_THEME_DIR_URL .'/css' ), '', t_em_theme( 'Version' ), 'all' );
+	wp_enqueue_style( 'twizr-' );
 }
-add_action( 'wp_enqueue_scripts', 't_em_child_enqueue' );
+add_action( 'wp_enqueue_scripts', 'twizr_enqueue' );
 
 /**
  * Dequeue styles form parent theme
  *
  * @since Twenty'em Child 1.2
  */
-function t_em_child_dequeue(){
+function twizr_dequeue(){
 	wp_dequeue_style( 'twenty-em-style' );
 	wp_deregister_style( 'twenty-em-style' );
 }
-add_action( 'wp_enqueue_scripts', 't_em_child_dequeue', 999 );
+add_action( 'wp_enqueue_scripts', 'twizr_dequeue', 999 );
 ?>
